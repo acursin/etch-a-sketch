6f5465623c2c72b8645ad3b984f9af521375cf09
alex <109973612+acursin@users.noreply.github.com>
2024-07-21 13:58:45 -0400

    Implement new sketchpad button

--- scripts.js
+++ scripts.js
@@ -1,5 +1,6 @@
 function createSketchpad(size) {
     const container = document.querySelector(".container");
+    container.replaceChildren();
     const fullSize = size * size
     const boxSize = 500 / size
 
@@ -7,8 +8,23 @@ function createSketchpad(size) {
         const box = document.createElement("div");
         box.classList.add("box");
         box.style.cssText = `width: ${boxSize}px; height: ${boxSize}px;`;
+        box.addEventListener("mouseenter", (event) => {
+            event.currentTarget.classList.add("filled");
+        });
         container.appendChild(box);
     }
 }
 
-createSketchpad(16);
+const STARTING_SIZE = 16
+const button = document.querySelector("button");
+button.addEventListener("click", () => {
+    let validSize = false;
+    let newSize = 0;
+    while (!validSize) {
+        newSize = prompt("Enter size from 1 to 100:", "");
+        if (newSize >= 1 && newSize <= 100) validSize = true;
+    }
+    createSketchpad(newSize);
+});
+
+createSketchpad(STARTING_SIZE);
